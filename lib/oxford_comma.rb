4554c5f21040e8b49558ba8586c8528779c6c8d0
Melissa Sames <melissa.sames@gmail.com>
2018-01-14 23:27:38 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
 def oxford_comma(array)
+  comma_string = ''
+  and_string = ''
 
+  if array.length <= 1
+    array.join
+  elsif array.length == 2
+    array.join(' and ')
+  else
+    array.each_with_index { |word, index|
+      if array[index] == array[-1]
+        and_string << "and #{word}"
+      else
+        comma_string << "#{word}, "
+      end
+    }
+    comma_string + and_string
+  end
 end
